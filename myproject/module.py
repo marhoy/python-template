a74594b31658a346ae17da299a74d16b5115ddcc
Martin Høy <marhoy@gmail.com>
2019-09-05 22:40:19 +0200

    Update

--- myproject/module.py
+++ myproject/module.py
@@ -1,10 +1,10 @@
 
-def add(a, b):
+def add(a: int, b: int) -> int:
     """Adds two numbers
 
     Args:
-        a (float): A number
-        b (float): Another number
+        a (int): A number
+        b (int): Another number
 
     Returns:
         The sum of the two numbers
@@ -14,3 +14,20 @@ def add(a, b):
         3
     """
     return a + b
+
+
+def sub(a: int, b: int) -> int:
+    """Subtract two numbers
+
+    Args:
+        a (int): A number
+        b (int: Another number
+
+    Returns:
+        The function returns a - b
+
+    Example:
+        >>> sub(2, 1)
+        1
+    """
+    return a - b
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,5 +1,9 @@
 import myproject.module
 
 
-def test_add():
+def test_add() -> None:
     assert myproject.module.add(3, 4) == 7
+
+
+def test_sub() -> None:
+    assert myproject.module.sub(10, 9) == 1
